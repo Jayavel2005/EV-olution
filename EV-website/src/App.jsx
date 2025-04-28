1d60cb91f22129f3bfd629c0c37c7da728b5601e
Jayavel <jayavel1520@gmail.com>
2025-04-28 22:39:17 +0530

    Navbar component finished

--- EV-website/src/App.jsx
+++ EV-website/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Background from "./Components/Background/Background.jsx"
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 const App = () => {
 
   let heroData = [
     { text1: "Dive into", text2: "what you love" },
-    { text1: "Indulge", text2: "your passions"},
-    {text1: "Give in to", text2: "your passions"}
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" }
   ];
 
   const [heroCount, setHeroCount] = useState(2);
@@ -14,7 +15,8 @@ const App = () => {
 
   return (
     <div>
-      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
     </div>
   )
 }
