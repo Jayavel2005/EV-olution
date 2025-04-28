69429e506949f8b1c74505f2081e42349a036513
Jayavel <jayavel1520@gmail.com>
2025-04-28 23:24:25 +0530

    landing page finished

--- EV-website/src/App.jsx
+++ EV-website/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "./Components/Background/Background.jsx"
 import Navbar from "./Components/Navbar/Navbar.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
 
 const App = () => {
 
@@ -10,13 +11,20 @@ const App = () => {
     { text1: "Give in to", text2: "your passions" }
   ];
 
-  const [heroCount, setHeroCount] = useState(2);
-  const [playStatus, setPlayStatus] = useState(true);
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => {return count === 2 ? 0 : count+1})
+    }, 3000)
+  }, [])
 
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount} />
       <Navbar />
+      <Hero setPlayStatus={setPlayStatus} heroData={heroData[heroCount]} heroCount={heroCount} setHeroCount={setHeroCount} playStatus={playStatus} />
     </div>
   )
 }
