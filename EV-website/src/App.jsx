e4cb1dc45f79d52fb0e345ed5ccb7f19b2495a5a
Jayavel <jayavel1520@gmail.com>
2025-04-28 22:25:43 +0530

    background component finished

--- EV-website/src/App.jsx
+++ EV-website/src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import Background from "./Components/Background/Background.jsx"
+
 const App = () => {
+
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions"},
+    {text1: "Give in to", text2: "your passions"}
+  ];
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(true);
+
   return (
     <div>
-      
+      <Background playStatus={playStatus} heroCount={heroCount}/>
     </div>
   )
 }
